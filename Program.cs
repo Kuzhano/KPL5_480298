832c52806691f5d386154ba3bda166f00046daef
kuzhano <sw30243@gmail.com>
2026-03-30 00:30:19 +0700

    commit message

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+
+        string nama = "Wawan Saputra";
+
+        halo.SapaUser<string>(nama);
+    }
+}
+
+
+public class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
